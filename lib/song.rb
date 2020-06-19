37488877325bda2003db3746e1093bfff7997989
Martin Achtermann <mdacht777@gmail.com>
2020-06-19 14:53:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,6 +27,11 @@ class Song
   end
 
   def self.genre_count
+#    @@artists.uniq.map { |x| [x, @@artists.count(x)] }.to_h
     @@genres.uniq.map { |x| [x, @@genres.count(x)] }.to_h
+  end
+
+  def self.artist_count
+    @@artists.uniq.map { |x| [x, @@artists.count(x)] }.to_h
   end
 end
